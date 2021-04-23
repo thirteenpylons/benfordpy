24c8eef3491d2a666a1728476197a77412675d63
[thirteen pylons] <[thirteenpylons@gmail.com]>
2021-04-23 17:11:10 -0400

    tests for benford

---
+++ test_benford.py
@@ -0,0 +1,18 @@
+"""
+Unit tests for module benford
+
+When run as a script, this module invokes several procedures
+that test the various functions in the module benford.
+
+Author:     Christian M. Fulton
+Date:       23/04/2021
+Modified:   23/04/2021
+"""
+
+import benford
+
+def test_firstNumber():
+    """
+    Test procedure for firstNumber
+    """
+    print("Testing firstNumber...")    
