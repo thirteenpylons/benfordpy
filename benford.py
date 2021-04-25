f9ba5799765c983b15e9d5d548c6891b3ee84782
freizch <[freizch@outlook.com]>
2021-04-24 22:18:53 -0400

    refactoring, conv local to global var

--- benford.py
+++ benford.py
@@ -7,6 +7,18 @@ Date: 23/04/2021
 import csv
 
 
+NUMBERS = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0
+    }
+
 def openDataset(file):
     """
     Used to open the dataset that will be used
@@ -34,34 +46,29 @@ def firstNumber(dataset):
 
     """
 
-    numbers = {
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-        7: 0,
-        8: 0,
-        9: 0
-        }
     for data in dataset:
         if str(data)[:1] == "1":
-            numbers[1] = numbers[1] + 1
+            NUMBERS[1] = NUMBERS[1] + 1
         elif str(data)[:1] == "2":
-            numbers[2] = numbers[2] + 1
+            NUMBERS[2] = NUMBERS[2] + 1
         elif str(data)[:1] == "3":
-            numbers[3] = numbers[3] + 1
+            NUMBERS[3] = NUMBERS[3] + 1
         elif str(data)[:1] == "4":
-            numbers[4] = numbers[4] + 1
+            NUMBERS[4] = NUMBERS[4] + 1
         elif str(data)[:1] == "5":
-            numbers[5] = numbers[5] + 1
+            NUMBERS[5] = NUMBERS[5] + 1
         elif str(data)[:1] == "6":
-            numbers[6] = numbers[6] + 1
+            NUMBERS[6] = NUMBERS[6] + 1
         elif str(data)[:1] == "7":
-            numbers[7] = numbers[7] + 1
+            NUMBERS[7] = NUMBERS[7] + 1
         elif str(data)[:1] == "8":
-            numbers[8] = numbers[8] + 1
+            NUMBERS[8] = NUMBERS[8] + 1
         elif str(data)[:1] == "9":
-            numbers[9] = numbers[9] + 1
-    return numbers
+            NUMBERS[9] = NUMBERS[9] + 1
+    return NUMBERS
+
+def percent():
+    s = sum(NUMBERS)
+    for k, v in NUMBERS.items():
+        pct = v * 100.0 / s
+        print(k, pct)
