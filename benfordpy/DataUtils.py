246ef41e3e1d409db63f0833d8c1950cd4e9ef7f
thirteenpylons <thirteenpylons@gmail.com>
2022-01-13 08:00:20 -0500

    adding ds

--- benfordpy/DataUtils.py
+++ benfordpy/DataUtils.py
@@ -1,19 +1,17 @@
 """
 Try this as OOP
 
-Using the Dataset class:
-    e
-
 Author: Christian M. Fulton
 Date: 31.Dec.2021
 Modified: 03.Jan.2022
 """
 import csv
 import pandas as pd
+from typing import AbstractSet, Iterator, ValuesView
 
 
 class Dataset:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
         
         self.dataset = self.readCsv(self.filename)
@@ -22,15 +20,23 @@ class Dataset:
         self.extracted_data = self.extractData()
 
     
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.dataset}"
 
-    def extractColumn(self, i):
+
+    def checkForEmbeddedList(self, data: list) -> bool:
+        """
+        Determine whether the data consists of multi dimensional list
+        """
+        return type(data[0]) == list
+        
+
+    def extractColumn(self, column_index: int):
         """
         Will extract columns that contain integer values.
 
-        Parameter i: Index of column to extract.
-        Precondition: i must be a non empty integer
+        Parameter column_index: Index of column to extract.
+        Precondition: column_index must be a non empty integer
         """
         NotImplementedError
 
@@ -122,7 +128,7 @@ class Dataset:
         return result
 
 
-    def readCsv(self, filename) -> list:
+    def readCsv(self, filename: str) -> list:
         """
         Returns the contents read from the CSV file filename as list.
         
@@ -141,9 +147,67 @@ class Dataset:
         return data
 
 
-    # TODO: calculate percent
-    def percent(numbers):
-        s = sum(numbers)
-        for k, v in numbers.items():
-            pct = v * 100.0 / s
-            print(k, pct)
+    # TODO: calculate percent --Should this be in a different class?
+    def getPercent(numbers: dict) -> dict:
+        """
+        @param numbers: dict of numbers to compute percentage
+        Precondition numbers: must be a dict
+        """
+        total = sum(numbers.values())
+        for key, value in numbers.items():
+            percent = value * 100.0 / total
+            print(key, percent)
+            
+
+    def keys(self) -> AbstractSet[BaseTag]:
+        """Return the :class:`Dataset` keys to simulate :meth:`dict.keys`.
+
+        Returns
+        -------
+        dict_keys
+            The :class:`~benfordpy.tag.BaseTag` of all the elements in
+            the :class:`Dataset`.
+        """
+        return self._dict.keys()
+
+
+    def values(self) -> ValuesView[_DatasetValue]:
+        """Return the :class:`Dataset` values to simulate :meth:`dict.values`.
+
+        Returns
+        -------
+        dict_keys
+            The :class:`DataElements<benfordpy.dataelem.DataElement>` that make
+            up the values of the :class:`Dataset`.
+        """
+        return self._dict.values()
+        
+
+    def __iter__(self) -> Iterator[DataElement]:
+        """Iterate through the top-level of the Dataset, yielding DataElements.
+
+        Examples
+        --------
+
+        >>> ds = Dataset()
+        >>> for elem in ds:
+        ...     print(elem)
+
+        The :class:`DataElements<benfordpy.dataelem.DataElement>` are returned in
+        increasing tag value order. Sequence items are returned as a single
+        :class:`~benfordpy.dataelem.DataElement`, so it is up
+        to the calling code to recurse into the Sequence items if desired.
+
+        Yields
+        ------
+        dataelem.DataElement
+            The :class:`Dataset`'s
+            :class:`DataElements<benfordpy.dataelem.DataElement>`, sorted by
+            increasing tag order.
+        """
+        # Note this is different than the underlying dict class,
+        #        which returns the key of the key:value mapping.
+        #   Here the value is returned (but data_element.tag has the key)
+        taglist = sorted(self._dict.keys())
+        for tag in taglist:
+            yield self[tag]
